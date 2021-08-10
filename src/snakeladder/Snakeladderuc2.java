55fc02c6d2fc128def10998c6c8ddb340e5e1cbd
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-08-11 00:37:45 +0530

    Player rolls the die

--- src/snakeladder/Snakeladderuc2.java
+++ src/snakeladder/Snakeladderuc2.java
@@ -7,12 +7,14 @@ package snakeladder;
  * @version 1.0
  * @since 10-08-2021
  */
-public class Snakeladderuc1 {
+public class Snakeladderuc2 {
     public static void main(String[] args) {
         System.out.println("Welconme to Snake-Ladder Game");
         System.out.println("Paly Single Player");
+        //declare int variable
         int startPosition = 0;
         System.out.println("Player start position is " + startPosition);
+        //Rolls the die get a number 1 to 6
         int dieCheck = 1 + (int)(Math.random() * ((6 -1) + 1));
         System.out.println("Player rolls the die and the number is - " +dieCheck);
     }
