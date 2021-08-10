d71472d54b247e3ccdbb7e51fe9ee031ab5c8189
sanjaypandit1999 <sanjay7001532411@gmail.com>
2021-08-11 00:17:44 +0530

    Singale Player play game start position is 0

---
+++ src/snakeladder/Snakeladderuc1.java
@@ -0,0 +1,17 @@
+package snakeladder;
+
+/**
+ * Snake ladder Game which play single player it's start position is 0
+ *
+ * @author Sanjay
+ * @version 1.0
+ * @since 10-08-2021
+ */
+public class Snakeladderuc1 {
+    public static void main(String[] args) {
+        System.out.println("Welconme to Snake-Ladder Game");
+        System.out.println("Paly Single Player");
+        int startPosition = 0;
+        System.out.println("Player start position is " + startPosition);
+    }
+}
